5878ec6638b2998480f758c81e05b9e634194278
quigongren <quigongren@gmail.com>
2025-11-22 02:44:32 -0500

    Add files via upload

---
+++ jfg_project_bootstrap/JFG_Project_Bootstrap.py
@@ -0,0 +1,12 @@
+# JFG Project Bootstrap Script
+
+ROOT_FOLDER = "[PASTE DRIVE FOLDER LINK]"
+SUBFOLDERS = [
+    "admin/Manifests",
+    "admin/Logs",
+    "data_public",
+    "docs",
+    "exports"
+]
+
+print("JFG Bootstrap initialized.")
